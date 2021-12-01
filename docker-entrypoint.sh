fd8361f06b9580e6f7bdf33366ef4510165994c7
John Adams <john@adams.xyz>
2021-12-01 15:03:35 -0500

    don't use globs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ cd libtpms-${LIBTPMS_VER}
 ./autogen.sh --with-openssl --with-tpm2
 dpkg-buildpackage -us -uc -j$(nproc)
 cd ..
-dpkg -i libtpms0_*_amd64.deb libtpms-dev_*_amd64.deb
+dpkg -i libtpms0_${LIBTPMS_VER}_amd64.deb libtpms-dev_${LIBTPMS_VER}_amd64.deb
 wget https://github.com/stefanberger/swtpm/archive/refs/tags/v${SWTPM_VER}.tar.gz
 tar xvf v${SWTPM_VER}.tar.gz
 cd swtpm-${SWTPM_VER}
